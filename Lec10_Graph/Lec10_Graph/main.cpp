6c24a0a76e35d810af7e3ee430026cd934127459
siwoo1313 <siwoo2762@gmail.com>
2026-05-15 10:44:10 +0900

    Lec10 파일 생성

---
+++ Lec10_Graph/Lec10_Graph/main.cpp
@@ -0,0 +1,31 @@
+#include "AdjMatGraph.h"
+#include <stdio.h>
+void main()
+{
+	AdjMatGraph g;
+
+	for (int i = 0; i < 4; i++)
+		g.insertVertex('A' + i);
+	g.insertEdge(0, 1);
+	g.insertEdge(0, 3);
+	g.insertEdge(1, 2);
+	g.insertEdge(1, 3);
+	g.insertEdge(2, 3);
+	printf("인접 행렬로 표현한 그래프\n");
+	g.display();
+
+	//파일 출력
+	FILE* fp;
+	fopen_s(&fp, "../text_out.txt", "w");
+	g.display(fp);
+	fclose(fp);
+
+	printf("그래프(graph.txt)\n");
+
+	g.display();
+	printf("DFS==>");
+	g.resetVisited();
+	g.DFS(0);
+	printf("\n");
+
+}
